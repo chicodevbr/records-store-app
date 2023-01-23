c1064810be624b9f61a1573cea816cfc910b8123
chicodevbr <chicodevbr@gmail.com>
2023-01-23 09:31:37 -0300

    feat: :sparkles: add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
+import Login from './Pages/Login';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
