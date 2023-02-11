a442f481dd12352134aa21cbdcf951ac3ba1283b
chicodevbr <chicodevbr@gmail.com>
2023-02-11 18:44:14 -0300

    add func addcart v1

--- src/Store/CartStore.ts
+++ src/Store/CartStore.ts
@@ -3,18 +3,39 @@ import { Record } from '../Types';
 
 const CartStore = () =>
   makeAutoObservable({
-    cart: [] as Record[],
+    items: [] as Record[],
+    list: {
+      items: [],
+      amount: Number,
+    },
 
-    add(_id: string, name: string, price: string) {
-      this.cart.push({
-        id: _id,
-        name: name,
-        price: price,
-      });
+    add(_id: string, name: string, price: number, amount: number) {
+      const existingCartItemIndex = this.items.findIndex(
+        (item) => item.id === _id
+      );
+      const existCartItem = this.items[existingCartItemIndex];
+
+      let updatedItems: Record[];
+
+      if (existCartItem) {
+        const updatedItem = {
+          ...existCartItem,
+          amount: existCartItem.amount + amount,
+        };
+        updatedItems = [...this.items];
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        this.items.push({
+          id: _id,
+          name: name,
+          price: price,
+          amount: amount,
+        });
+      }
     },
 
     remove(record: Record) {
-      this.cart = this.cart.filter((r) => r.id !== record.id);
+      this.items = this.items.filter((r) => r.id !== record.id);
     },
   });
 
